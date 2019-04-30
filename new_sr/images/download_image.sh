f1ceb422d605151d91806e5606f3a827d47fd5d8
gjpicker <724188300@qq.com>
2019-04-30 18:31:14 +0800

    ad  dow sh

---
+++ new_sr/images/download_image.sh
@@ -0,0 +1,8 @@
+
+echo ""
+echo ""
+echo "****************** ATOM Network ******************"
+bash ../utils/gdrive_download 1-uHtlQQB9uezko_GJYiWsYFCEiBn8ZsS ./backup.tar 
+tar -xvf ./backup.tar 
+
+
